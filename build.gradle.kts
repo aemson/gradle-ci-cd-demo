e0f7458dbe986847e02264804fb12ea76804565a
Aemson Michael <aemson.michael@yara.com>
2022-10-18 18:33:15 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,12 @@ val quarkusPlatformVersion: String by project
 
 dependencies {
     implementation(enforcedPlatform("$quarkusPlatformGroupId:$quarkusPlatformArtifactId:$quarkusPlatformVersion"))
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson:2.10.4.Final")
     implementation("io.quarkus:quarkus-resteasy-reactive")
     implementation("io.quarkus:quarkus-kotlin:2.11.1.Final")
     implementation("io.quarkus:quarkus-amazon-lambda-http:$quarkusPlatformVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-arc:2.11.0.Final")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
