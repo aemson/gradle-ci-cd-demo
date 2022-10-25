032e9913dc62c38a3fab6d17e5c4dbcd82195a1d
AemsonM <aemson.michael@yara.com>
2022-10-25 14:47:34 +0200

    lazy commit message

--- src/main/kotlin/com/service/CalculatorService.kt
+++ src/main/kotlin/com/service/CalculatorService.kt
@@ -1,8 +1,6 @@
 package com.service
 
-data class CalculatorInputs(val num1: Int, val num2: Int, val operation: String) {
-    constructor() : this(0, 0, "plus")
-}
+data class CalculatorInputs(val num1: Int, val num2: Int, val operation: String)
 
 fun calculate(num1: Int, num2: Int, function: (Int, Int) -> Int): String = "Output is ${function(num1, num2)}"
 
